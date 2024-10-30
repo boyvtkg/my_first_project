4ff99f176e736c9f0fef8553a60a3d4689cd5e78
boyvtkg <quyle0711@gmail.com>
2024-10-30 14:15:59 -0700

    Change Style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const handleLoginClick = () => {
-    // Navigate to the separate /login page
+    // Navigate to /login page
     navigate('/login');
   };
 
@@ -30,7 +30,7 @@ const Navbar = () => {
         setIsSticky(true);
         setIsMenuOpen(false)
       }
-      else{
+      else {
         setIsSticky(false);
       }
     };
@@ -43,32 +43,31 @@ const Navbar = () => {
   }, []);
 
   const navItems = [
-    {link: "Home", path: "home" },
-    {link: "Service", path: "service" },
-    {link: "About", path: "about" },
-    {link: "Product", path: "product" },
-    {link: "Testimonial", path: "testimonial" },
-    {link: "FAQ", path: "faq" },
+    { link: "Home", path: "home" },
+    { link: "Service", path: "service" },
+    { link: "About", path: "about" },
+    { link: "Product", path: "product" },
+    { link: "Testimonial", path: "testimonial" },
+    { link: "FAQ", path: "faq" },
   ];
   return (
     <header className=" bg-white md:bg-transparent fixed top-0 left-0 right-0">
-      <nav className={`py-4 lg:px-14 px-4 ${
-          isSticky ? "sticky top-0 right-0 left-0 border bg-white transition-all duration-300" : ""
+      <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 right-0 left-0 border bg-white transition-all duration-300" : ""
         }`}>
         <div className="flex justify-between items-center text-base gap-8">
-          <a href="" className="text-2xl font-semibold flex items-center space-x-3"><img src={logo} alt="" className="w-10 inline-block items-center"/><span>Word of Life</span></a>
+          <a href="" className="text-2xl font-semibold flex items-center space-x-3"><img src={logo} alt="" className="w-10 inline-block items-center" /><span>Word of Life</span></a>
 
-          <ul className="md:flex space-x-12 hidden">          
-          {
-            navItems.map(({link, path}) => <Link to={path} spy={true} smooth={true} offset={-100} key={link} href={path} 
-              className="block text-base text-gray900 hover:text-brandPrimary first:font-medium">
-              {link}
-            </Link> )
-          }
+          <ul className="md:flex space-x-12 hidden">
+            {
+              navItems.map(({ link, path }) => <Link to={path} spy={true} smooth={true} offset={-100} key={link} href={path}
+                className="block text-base text-gray900 hover:text-brandPrimary first:font-medium">
+                {link}
+              </Link>)
+            }
           </ul>
 
           <div className="space-x-12 hidden lg:flex items-center">
-            
+
             <button onClick={handleLoginClick} className="bg-brandPrimary text-white  py-2 px-4 transition-all duration-300 
               rounded hover:bg-neutralDGrey">Login
             </button>
@@ -80,29 +79,25 @@ const Navbar = () => {
               onClick={toggleMenu}
               className="text-gray900 focus:outline-none focus:text-gray-500"
             >
-              {isMenuOpen ? (
-                <FaXmark  className="h-6 w-6 text-primary"/>
-              ) : (
-                <FaBars className="h-6 w-6 text-primary" />
-              )}
+              {isMenuOpen ? (<FaXmark className="h-6 w-6 text-primary" />) :
+                (<FaBars className="h-6 w-6 text-primary" />)
+              }
             </button>
           </div>
         </div>
 
-        <div
-        className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"}`}
-      >
-        {
-                navItems.map(({link, path}) => <Link 
-                to={path} spy={true} smooth={true} offset={-90}
-                key={link} 
-                onClick={toggleMenu}
-                className="block  text-white hover:text-gray-500"
-                >
-                {link}
-              </Link> )
-            }
-      </div>
+        <div className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary 
+        ${isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"}`}>
+          {
+            navItems.map(({ link, path }) => <Link
+              to={path} spy={true} smooth={true} offset={-90}
+              key={link} onClick={toggleMenu}
+              className="block  text-white hover:text-gray-500"
+            >
+              {link}
+            </Link>)
+          }
+        </div>
       </nav>
     </header>
   );
