ad0fdd19617ab59c8dd89423ea3fb733e644ec24
boyvtkg <quyle0711@gmail.com>
2024-11-13 11:18:55 -0800

    Nothing

--- client/src/components/mainComponents/About.jsx
+++ client/src/components/mainComponents/About.jsx
@@ -11,23 +11,23 @@ import { fadeIn } from "../../variants";
 const About = () => {
   return (
     <div>
-        {/* about text */}
+      {/* about text */}
       <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-8" id="about">
         <div className="md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12">
           <motion.div
-          variants={fadeIn("right", 0.2)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: false, amount: 0.6 }}
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.6 }}
           >
-            <img src={aboutImg} alt="" className="w-full"/>
+            <img src={aboutImg} alt="" className="w-full" />
           </motion.div>
           <motion.div
-          variants={fadeIn("left", 0.3)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: false, amount: 0.5 }}
-          className="md:w-3/5 mx-auto">
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.5 }}
+            className="md:w-3/5 mx-auto">
             <h2 className="text-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5">
               The unseen of spending three years at Pixelgrade
             </h2>
@@ -46,54 +46,54 @@ const About = () => {
 
       {/* company stats */}
       <motion.div
-      variants={fadeIn("up", 0.2)}
-      initial="hidden"
-      whileInView={"show"}
-      viewport={{ once: false, amount: 0.7 }}
-      className="px-4 lg:px-14 max-w-screen-2xl mx-auto bg-neutralSilver py-16">
+        variants={fadeIn("up", 0.2)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.7 }}
+        className="px-4 lg:px-14 max-w-screen-2xl mx-auto bg-neutralSilver py-16">
         <div className="flex flex-col md:flex-row justify-between items-center gap-8">
-            <div className="md:w-1/2">
+          <div className="md:w-1/2">
             <h2 className="text-4xl text-neutralDGrey font-semibold mb-2 md:w-2/3">
-            Helping a local <br /> <span className="text-brandPrimary">business reinvent itself</span>
+              Helping a local <br /> <span className="text-brandPrimary">business reinvent itself</span>
             </h2>
             <p>We reached here with our hard work and dedication</p>
-            </div>
+          </div>
 
-            {/* stats */}
-            <div className="md:w-1/2 mx-auto flex sm:flex-row flex-col sm:items-center justify-around gap-12">
-                <div className="space-y-8">
-                <div className="flex items-center gap-4">
-                    <img src="/src/assets/icons/group.png" alt="" />
-                    <div>
-                        <h4 className="text-2xl text-neutralDGrey font-semibold">2,245,341</h4>
-                        <p>Members</p>
-                    </div>
+          {/* stats */}
+          <div className="md:w-1/2 mx-auto flex sm:flex-row flex-col sm:items-center justify-around gap-12">
+            <div className="space-y-8">
+              <div className="flex items-center gap-4">
+                <img src="/src/assets/icons/group.png" alt="" />
+                <div>
+                  <h4 className="text-2xl text-neutralDGrey font-semibold">2,245,341</h4>
+                  <p>Members</p>
                 </div>
-                <div className="flex items-center gap-4">
-                    <img src="/src/assets/icons/clubs.png" alt="" />
-                    <div>
-                        <h4 className="text-2xl text-neutralDGrey font-semibold">46,328</h4>
-                        <p>Clubs</p>
-                    </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <img src="/src/assets/icons/clubs.png" alt="" />
+                <div>
+                  <h4 className="text-2xl text-neutralDGrey font-semibold">46,328</h4>
+                  <p>Clubs</p>
                 </div>
-                </div>
-                <div className="space-y-8">
-                <div className="flex items-center gap-4">
-                    <img src="/src/assets/icons/click.png" alt="" />
-                    <div>
-                        <h4 className="text-2xl text-neutralDGrey font-semibold">828,867</h4>
-                        <p>Event Bookings</p>
-                    </div>
-                </div>
-                <div className="flex items-center gap-4">
-                    <img src="/src/assets/icons/payments.png" alt="" />
-                    <div>
-                        <h4 className="text-2xl text-neutralDGrey font-semibold">1,926,436</h4>
-                        <p>Payments</p>
-                    </div>
-                </div>
-                </div>
+              </div>
             </div>
+            <div className="space-y-8">
+              <div className="flex items-center gap-4">
+                <img src="/src/assets/icons/click.png" alt="" />
+                <div>
+                  <h4 className="text-2xl text-neutralDGrey font-semibold">828,867</h4>
+                  <p>Event Bookings</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <img src="/src/assets/icons/payments.png" alt="" />
+                <div>
+                  <h4 className="text-2xl text-neutralDGrey font-semibold">1,926,436</h4>
+                  <p>Payments</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </motion.div>
 
